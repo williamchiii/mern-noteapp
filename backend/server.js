71fb61ee1c1d342d599217a43ae810ab65bf61de
w chi <wchi@yahoo>
2025-12-22 23:11:20 -0500

    created backend api endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,26 @@ const express = require("express"); //require the express pacakge, basically lik
 
 const app = express(); //api initilization
 
-app.get("/api/notes", (req, res) => {
-    res.send("testing purposes, it works");
-}); //get from location, then when we get, run (http://localhost:5001/api/notes)
+http://localhost:5001/api/notes
+//get endpoint
+app.get("/api/notes", (req, res) => { 
+    res.status(200).send("testing purposes, it does works");
+});
+
+//create endpoint
+app.post("/api/notes", (req, res) => {
+    res.status(201).send.json({message: "note created successfully"});
+});
+
+//update endpoint
+app.put("/api/notes", (req, res) => {
+    res.status(200).send.json({message: "note updated successfully"});
+});
+
+//delete endpoint
+app.delete("/api/notes", (req, res) => {
+    res.status(200).send.json({message: "note deleted successfully"});
+});
 
 app.listen(5001, () => {
     console.log("Server Started on PORT: 5001");
