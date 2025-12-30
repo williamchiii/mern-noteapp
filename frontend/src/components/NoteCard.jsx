66d1e3b14be771821f22518a9e1d97abd903a87f
William Chi <chi.will@outlook.com>
2025-12-30 11:16:53 -0500

    note delete page created

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,23 +1,40 @@
 import { PenSquareIcon, Trash2Icon } from "lucide-react"
 import {Link} from "react-router"
 import {formatDate} from "../lib/utils.js"
+import api from "../lib/axios.js"
+import toast from "react-hot-toast"
 
-const NoteCard = ({note}) => {
+const NoteCard = ({note, setNotes}) => {
+    const handleDelete = async (e, id) => {
+      
+        e.preventDefault(); //get rid of the navigtaion behavior
+
+        if(!window.confirm("Are you sure you want to delete your note?")) return;
+
+        try{
+            await api.delete(`/notes/${id}`)
+            toast.success("Note deleted successfully")
+            setNotes((prev)=>prev.filter(note => note._id !== id)) //get rid of the deleted one 
+        } catch(error){
+            console.log("error in handleDelete", error)
+            toast.error("Failed to delete note")
+        }
+    }  
   return (
-    <Link to = {'/note/${note._id}'}
+    <Link to = {`/note/${note._id}`}
         className="card bg-base-100 hover:shadow-lg transition-all duration-200
-        border-t-4 border-solid border-[#00FF9D]" 
+        border-t-4 border-solid border-[#39bdf8]" 
         >
         <div className="card-body">
-            <h3 className="card-title text-based-content">{note.title}</h3>
-            <p className="text-based-content/70 line-clamp-3">{note.content}</p>
+            <h3 className="card-title text-base-content">{note.title}</h3>
+            <p className="text-base-content/70 line-clamp-3">{note.content}</p>
             <div className="card-actions justify-between items-center mt-4">
-                <span className="text-sm text-based-content/60">
+                <span className="text-sm text-base-content/60">
                     {formatDate(new Date(note.createdAt))}
                 </span>
                 <div className="flex items-center gap-1">
                     <PenSquareIcon className="size-4" />
-                    <button className="btn btn-ghost btn-xs text-error">
+                    <button type="button" className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
                         <Trash2Icon className="size-4" />
                     </button>
                 </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ import { useEffect } from "react";
 import api from "../lib/axios.js"
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard.jsx";
+import NotesNotFound from "../components/NotesNotFound.jsx";
+
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -39,10 +41,12 @@ const HomePage = () => {
     <div className="max-w-7xl mx-auto p-4 mt-6">
       {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
+      {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
       {notes.length > 0 && !isRateLimited && (
         <div className="grid gridcols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {notes.map(note => (
-            <NoteCard key={note._id} note={note}/>
+            <NoteCard key={note._id} note={note} setNotes={setNotes}/>
           ))}
         </div>
       )}
