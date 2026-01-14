67eec42bfcd58b223e76262f50938d23c889b2dc
William Chi <chi.will@outlook.com>
2026-01-14 11:29:52 -0500

    fix: correct condition for note not found condition (gets rid of the split second NotNotFound condition when notes are loading)

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -41,7 +41,7 @@ const HomePage = () => {
     <div className="max-w-7xl mx-auto p-4 mt-6">
       {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
-      {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+      {notes.length === 0 && !isRateLimited &&!loading && <NotesNotFound />}
 
       {notes.length > 0 && !isRateLimited && (
         <div className="grid gridcols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
